1007112c9772df2df2f6e38fd86bbef261dce728
Daniel Wippermann <danielw83@live.de>
2022-05-29 17:09:40 +0200

    first commit

---
+++ EmojiArt/Model/EmojiArtModel.Background.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Daniel Wippermann on 29.05.22.
+//
+
+import Foundation
---
+++ EmojiArt/Model/EmojiArtModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Daniel Wippermann on 29.05.22.
+//
+
+import Foundation
