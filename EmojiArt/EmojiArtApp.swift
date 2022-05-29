6204cc733d30a06d8ef025dbccd42839a8e2c87b
Daniel Wippermann <danielw83@live.de>
2022-05-29 16:36:34 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Daniel Wippermann on 29.05.22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
